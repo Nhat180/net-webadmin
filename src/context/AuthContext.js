b29e0c5f37b0e39b9647c9ab2133cb7867698f87
Lenovo <cuong.hs96@gmail.com>
2022-07-14 11:31:54 +0700

    Cuong auth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,14 +5,57 @@ import {
   onAuthStateChanged,
 } from 'firebase/auth';
 import { auth } from '../firebase';
+import { db } from '../firebase';
+import {getDoc, doc} from 'firebase/firestore'
+// import * as Uri from "postcss";
+// import * as http from "http";
 
 const UserContext = createContext();
+const api = "https://netcompany-crawl-server.herokuapp.com/auth";
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
-   const signIn = (email, password) =>  {
-    return signInWithEmailAndPassword(auth, email, password)
+   const signIn = async (email, password) => {
+     if (email.toString() === "admin") {
+       email += "@gmail.com"
+       return signInWithEmailAndPassword(auth, email, password)
+     }
+
+     // const docRef = doc(db, "users", email);
+     // console.log(docRef)
+     // const docSnap = await getDoc(docRef);
+     // console.log(docSnap)
+     //
+     // if(!docSnap.exists()){
+     //   return signInWithEmailAndPassword(auth, email, "wrongpass")
+     // }
+     // const isAdmin = JSON.parse(docSnap.data())["isAdmin"]
+     // if(isAdmin !== false){
+     //   return signInWithEmailAndPassword(auth, email, "wrongpass")
+     // }
+
+     const response = await fetch(api, {
+       method: 'POST',
+       headers: {'Content-Type': 'application/json'},
+       body: JSON.stringify({
+         "username": email,
+         "password": password
+       })
+     })
+     console.log(response)
+
+
+     email += "@gmail.com"
+
+     if(response.status === 500 || response.status === 200){
+       return signInWithEmailAndPassword(auth, email, "123456")
+     }else{
+       return signInWithEmailAndPassword(auth, email, "wrongpass")
+     }
+
+     // return signInWithEmailAndPassword(auth, email, "123456")
+
    }
 
   const logout = () => {
