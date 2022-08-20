03d7009c92a24e9d04902556f4c31108ebc255d2
Lenovo <cuong.hs96@gmail.com>
2022-08-20 07:04:17 +0700

    update create survey

--- src/components/SurveyCreate/index.js
+++ src/components/SurveyCreate/index.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Accordion, Button, FormControl, Nav, Spinner } from "react-bootstrap";
 import{ db } from "../../firebase";
-import { doc, getDoc, collection, connectFirestoreEmulator} from "firebase/firestore";
+import { doc, getDoc, collection, connectFirestoreEmulator, Timestamp} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { SignIn } from "../SignIn/index";
 import Sidebar from '../Sidebar.jsx'
 import {setDoc, addDoc} from 'firebase/firestore'
+import {  Routes, Route } from "react-router-dom";
 import "../../Sidebar.css"
 import "./surveyCreate.css"
+import { useNavigate } from "react-router-dom";
 import { AccordionSummary, AccordionDetails, IconButton } from "@mui/material";
 import { Typography } from "antd";
 import { style, width } from "@mui/system";
@@ -17,6 +20,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import {BsTrash} from 'react-icons/bs';
 import FilterNoneIcon from '@mui/icons-material/FilterNone';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+import { async } from '@firebase/util';
 
 export default function SurveyCreate() {
     const [questions, setQuestions] = useState(
@@ -32,6 +36,9 @@ export default function SurveyCreate() {
             open: true
         }]
     )
+
+    let navigate = useNavigate();
+    let auth = getAuth();
 
     const [surveyName, setSurveyName] = useState();
     const [surveyDeadline, setSurveyDeadline] = useState();
@@ -75,7 +82,7 @@ export default function SurveyCreate() {
     function addOption(i){
         var optionQuestion = [...questions];
         if(optionQuestion[i].options.length < 5){
-            optionQuestion[i].options.push({optionText: "Option " + (optionQuestion[i].options.length + 1)});
+            optionQuestion[i].options.push({optionText: "" + (optionQuestion[i].options.length + 1)});
             setQuestions(optionQuestion);
         }
     }
@@ -98,12 +105,84 @@ export default function SurveyCreate() {
 
     function addQuestion(i){
         var qs = [...questions];
-        qs.splice(i + 1, 0, {questionText: "Question", questionType:"radio", options:[{optionText:"Option 1"}], open: true})
+        qs.splice(i + 1, 0, {questionText: "", questionType:"radio", options:[{optionText:""}], open: true})
         setQuestions(qs)
     }
 
-    function submitsurvey(){
+    async function submitsurvey(){
+        if(surveyName == null){
+            return (alert("Survey name must be filled"))
+        }
+        if(surveyDeadline == null){
+            return (alert("Survey deadline must be set"))
+        }
+        for(let i = 0; i < questions.length; i ++){
+            if(questions[i].questionText == ""){
+                return (alert("All question name must be set"))
+            }
+            for (let j = 0; j < questions[i].options.length; j ++){
+                if(questions[i].options[j].optionText == ""){
+                    return (alert("All option text must be filled"))
+                }
+            }
+        }
 
+        let type = ["radio", "checkbox", "text", "date", "likert"];
+        let fbType = ["SINGLECHOICE", "MULTIPLECHOICE", "SHORTANSWER", "DATEPICK", "LIKERTSCALE"];
+        let user = auth.currentUser.email.slice(0, auth.currentUser.email.length - 10);
+        let surveyID = Timestamp.fromMillis(Date.parse(surveyDeadline)).toString;
+        var docData = {
+            title: surveyName,
+            close: Timestamp.fromMillis(Date.parse(surveyDeadline)),
+            created: Timestamp.fromDate(new Date()),
+            createdBy: user + "",
+            usersHaveTaken: []
+        }
+        let svdoc = await addDoc(collection(db, "surveys"), docData);
+        
+        // const docRef = doc(db, "surveys", surveyID);
+        // setDoc(docRef, docData);
+        for(let i = 0; i < questions.length; i ++){
+            var quesType = "SINGLECHOICE"
+            for (let j = 0; j < type.length; j++){
+                if(type[j] == questions[i].questionType){
+                    quesType = fbType[j];
+                }
+            }
+
+            var quesData = {
+                title: questions[i].questionText + "",
+                type: quesType + ""
+            }
+            await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText), quesData);
+
+            if(questions[i].questionType == "radio" || questions[i].questionType == "checkbox"){
+                for(let j = 0; j < questions[i].options.length; j ++){
+                    var ansData = {
+                        choiceCount: 0,
+                        title: questions[i].options[j].optionText + ""
+                    }
+                    let awid = j + ""
+                    await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", awid), ansData);
+                }
+            } else if(questions[i].questionType == "likert"){
+                var ansData = {
+                    choiceCount: 0,
+                    title: "Terrible"
+                }
+                await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", "0"), ansData);
+                ansData.title = "Bad";
+                await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", "1"), ansData);
+                ansData.title = "Neutral";
+                await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", "2"), ansData);
+                ansData.title = "Good";
+                await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", "3"), ansData);
+                ansData.title = "Satisfied";
+                await setDoc(doc(db, "surveys", svdoc.id, "questions", questions[i].questionText, "answers", "4"), ansData);
+
+            }
+        }
+        return console.log("test");
     }
 
     function questionUI(){
@@ -113,7 +192,7 @@ export default function SurveyCreate() {
         return questions.map((ques, i)=>
             <div>
                 <Accordion expanded = {questions[i].open} className={questions[i].open ? 'add_border': ""}>
-                    <AccordionSummary  elevation={1} style={{width:'100%'}}>
+                    {/* <AccordionSummary  elevation={1} style={{width:'100%'}}>
                         {!questions[i].open ? (
                              <div className="saved_question">
                                 <Typography style={{fontSize:"15px", fontWeight:"400", letterSpacing: '.1px', lineHeight:'24px', paddingBottom:"8px"}}>
@@ -140,17 +219,18 @@ export default function SurveyCreate() {
                                 )} 
                             </div>
                          ): ""}
-                    </AccordionSummary>
+                    </AccordionSummary> */}
 
                     <div className="question_boxes">
                         <AccordionDetails className="add_question">
                             <div className="add_question_top">
                                 <input type="text" className="question" placeholder="Question" value={ques.questionText} onChange={(e)=>{changeQuestion(e.target.value, i)}}></input>
-                                <Select className="select" style={{color:"#5f6368", fontSize:"13px"}} value={ques.questionType}>
-                                    <MenuItem id="text" value="Text" onClick={()=>{addQuestionTypeNoOption(i, "text")}}> Text input</MenuItem>
-                                    <MenuItem id="likert" value="Likert" onClick={()=>{addQuestionTypeNoOption(i, "likert")}}> Rating</MenuItem>
-                                    <MenuItem id="checkbox" value="Checkbox" onClick={()=>{addQuestionType(i, "checkbox")}}> Multiple choice</MenuItem>
+                                <Select className="select" style={{fontSize:"13px"}} defaultValue={ques.questionType}>
                                     <MenuItem id="radio" value = "Radio" onClick={()=>{addQuestionType(i, "radio")}}> Single choice</MenuItem>
+                                    <MenuItem id="checkbox" value="Checkbox" onClick={()=>{addQuestionType(i, "checkbox")}}> Multiple choice</MenuItem>
+                                    <MenuItem id="text" value="Text" onClick={()=>{addQuestionTypeNoOption(i, "text")}}> Text input</MenuItem>
+                                    <MenuItem id="date" value="Date" onClick={()=>{addQuestionTypeNoOption(i, "date")}}> Date pick</MenuItem>
+                                    <MenuItem id="likert" value="Likert" onClick={()=>{addQuestionTypeNoOption(i, "likert")}}> Rating</MenuItem>
                                 </Select>
 
                             </div>
@@ -226,9 +306,9 @@ export default function SurveyCreate() {
                     <br/>
                     <div className = "section">
                         <div className = "top">
-                            <input type="text" className="surveyname" style={{color:"black"}} placeholder="Untitled survey" ></input>
+                            <input type="text" className="surveyname" style={{color:"black"}} placeholder="Untitled survey" onChange={(e)=> setSurveyName(e.target.value)}></input>
                             <p style={{textAlign: 'left', marginTop: 20, fontSize: 20, marginBottom: -5}}>Expired date</p>
-                            <input type="date" className="deadline" style={{width:"100px", marginRight:"600px", marginTop:"20px"}}></input>
+                            <input type="datetime-local" className="deadline" style={{width:"150px", marginRight:"600px", marginTop:"20px"}} onChange={(e)=>setSurveyDeadline(e.target.value)}></input>
 
 
                             
